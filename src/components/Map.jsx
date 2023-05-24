32fff87d4d84cee6bda19650c50d4b152dca162a
Cadufc91 <fernandes.cadu@gmail.com>
2023-05-24 11:10:34 -0300

    feat: map drawing added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
-
-const center = [-23.572218119099436, -46.632395960507004];
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, FeatureGroup } from 'react-leaflet';
+import { EditControl } from 'react-leaflet-draw';
 
 export const Map = () => {
+    const center = [-23.572218119099436, -46.632395960507004];
+    const [mapLayers, setMapLayers] = useState([]);
+
+    const _onCreated = (e) => {
+        console.log(e);
+
+        const { layerType, layer } = e;
+        if(layerType === "polygon") {
+            const {_leaflet_id} = layer;
+            setMapLayers(layers => [
+                ...layers, { id: _leaflet_id, latlngs: layer.getLatLngs()[0]}
+            ]);
+        }
+    };
+
+    const _onEdited = (e) => {
+        console.log(e);
+    };
+
+    const _onDeleted = e => {
+        console.log(e);
+    }
+
   return (
     <div id='map'>
         <MapContainer 
@@ -12,12 +34,30 @@ export const Map = () => {
             zoom={10}
             scrollWheelZoom={false}
         >
+            <FeatureGroup>
+                <EditControl
+                    position='topright'
+                    onCreated={_onCreated}
+                    onEdited={_onEdited}
+                    onDeleted={_onDeleted}
+                    draw={{
+                        rectangle: false,
+                        polyline: false,
+                        circle: false,
+                        circlemarker: false,
+                        marker: false,
+                    }}
+                />
+            </FeatureGroup>
+
             <TileLayer 
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             
         </MapContainer>
+
+        <p>{JSON.stringify(mapLayers, 0, 2)}</p>
     </div>
   )
 }
